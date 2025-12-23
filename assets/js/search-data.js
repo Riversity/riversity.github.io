7c88ceae15881a098f833e0c967ca6a9de1d309e
Riversity <142235272+Riversity@users.noreply.github.com>
2025-12-23 08:21:45 +0000

    Deploying to gh-pages from @ Riversity/riversity.github.io@87b691eaab7c0baa0433ef5794db2acad0197446 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-blog",
-          title: "blog",
+          title: "Blog",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -35,14 +35,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/cv.pdf", "_blank");
+          window.open("/CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%6D%61%61%79%69%68%6A%69%61＠%73%6A%74%75・%65%64%75・%63%6E", "_blank");
+          window.open("mailto:%6D%61%61%79%69%68%6A%69%61@%73%6A%74%75.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-github',
